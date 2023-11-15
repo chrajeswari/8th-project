ee84225bc8b5af5f08f2a59112077a29439d30cc
chrajeswari <ch.raji@32003@gmail.com>
2023-11-15 22:27:45 +0530

    first commit

---
+++ africa/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def shreyas(request):
+    return HttpResponse('<h1>shreyas is the best plyer</h1>')
